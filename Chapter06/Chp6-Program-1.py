1a158ac45a7315efb9f5fc2bdb7878a668714446
utkarsha-Packt <utkarshak@packt.com>
2019-06-27 19:30:12 +0530

    Add files via upload

---
+++ Chapter06/Chp6-Program-1.py
@@ -0,0 +1,11 @@
+import json
+from ibm_watson import NaturalLanguageClassifierV1
+
+service = NaturalLanguageClassifierV1(
+        url='https://gateway.watsonplatform.net/natural-language-classifier/api',
+        iam_apikey='JieYHJwBRgrd5Rl9R4q63d5DWvAuffdrRIj1jKhkfoAH')
+
+classes = service.classify('8a423bx518-nlc-1830',
+        'Airline lost my luggage twice in the same month, delayed my flight, ' + \
+        'and didn\'t care about my stopover!').get_result()
+print(json.dumps(classes, indent=2))
---
+++ Chapter06/Chp6-Program-2.py
@@ -0,0 +1,14 @@
+import json
+from ibm_watson
+import NaturalLanguageClassifierV1
+
+service = NaturalLanguageClassifierV1(
+    url='https://gateway.watsonplatform.net/natural-language-classifier/api',
+    iam_apikey='JieYHJwBRgrd5Rl9R4q63d5DWvAuffdrRIj1jKhkfoAH')
+
+user_tweet = input("Give me a tweet: ")
+classes = service.classify('8a423bx518-nlc-1830', user_tweet).get_result()
+
+for sentiment in classes["classes"]:
+    print(sentiment["class_name"] + " sentiment: " +
+          str(round(sentiment["confidence"] * 100)) + "% confidence")
